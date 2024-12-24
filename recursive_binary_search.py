667ccb4443a0b64e7e898379b28880396e004c97
Drew <ayobamioduola13@gmail.com>
2024-12-24 06:22:03 +0100

    recursive_binary_search.py commented

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -1,16 +1,21 @@
 def recursive_binary_search(list1, target):
-    if len(list1) == 0:
+    """This function work just like the binary search 
+    but with recursion. It starts by checking if the list is empty. If it is, it returns False.
+    If the list isn't empty, it finds the midpoint of the list and compares it to the target.
+    If they're equal, it returns True. If the midpoint is less than the target, it recursively calls itself on the right(or left) half of the list.
+    """
+    if len(list1) == 0: # Check if the list is empty
         return False
     else:
-        midpoint = (len(list1))//2
+        midpoint = (len(list1))//2 # Get the midpoint of the list
 
-        if list1[midpoint] == target:
+        if list1[midpoint] == target: # Check if the midpoint is equal to the target
             return True
         else:
-            if list1[midpoint] < target:
-                return recursive_binary_search(list1[midpoint+1:], target)
+            if list1[midpoint] < target: # if the midpoint is less than the target
+                return recursive_binary_search(list1[midpoint+1:], target) # Recursive binary search on the right side of the list
             else:
-                return recursive_binary_search(list1[:midpoint], target)
+                return recursive_binary_search(list1[:midpoint], target) # Recursive binary search on the left side of the list
             
 def verify(result):
     print("Target found: ", result)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,17 @@
-my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+# Recursive Binary Search
+
+def recursive_binary_search(array, target):
+    if len(array) == 0:
+        return False
+    else:
+        midpoint = len(array) // 2
+
+        if array[midpoint] == target:
+            return True
+        elif array[midpoint] < target:
+            return recursive_binary_search(array[midpoint+1:], target)
+        else:
+            return recursive_binary_search(array[:midpoint], target)
+        
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(recursive_binary_search(my_array, 11))
